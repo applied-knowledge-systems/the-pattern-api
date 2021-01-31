b60347c8099096dd349d113518376f1fd9c2ce95
Alex Mikhalev <alex@sci-blog.com>
2021-01-31 16:22:17 +0000

    start reverted to local example
    Signed-off-by: Alex Mikhalev <alex@sci-blog.com>

--- start.sh
+++ start.sh
@@ -2,7 +2,5 @@
 app="the-pattern-api"
 network_name='redis_cluster_net'
 docker build -t ${app} .
-docker run -d --rm -p 85.10.241.154:8181:8181 --name=${app} --net $network_name ${app}
-# docker run -d -p 8181:8181 \
-#   --name=${app} --net $network_name\
-#   -v $PWD:/app ${app}
+# docker run -d --rm -p 85.10.241.154:8181:8181 --name=${app} --net $network_name ${app}
+docker run -d -p 8181:8181 --name=${app} --net $network_name -v $PWD:/app ${app}
