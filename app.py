474aebd563201a327a07f39cb2e01556d4927b43
Alex Mikhalev <alex@sci-blog.com>
2021-04-05 19:24:37 +0100

    Put back QA search
    Signed-off-by: Alex Mikhalev <alex@sci-blog.com>

--- app.py
+++ app.py
@@ -79,36 +79,36 @@ def gsearch_task():
     node_list=[{'name':k,'id':node_dict[k]['id'],'rank':node_dict[k]['rank']} for k in node_dict]
     return jsonify({'nodes': node_list,'links': links,'years':years_list}), 200
 
-# from qasearch.qa_bert import *
-# @app.route('/qasearch', methods=['POST'])
-# def qasearch_task():
-#     """
-#     this search using Redis Graph to get list of articles and sentences and then calls BERT QA model to create answer
-#     TODO: pre-process articles with qa tokeniser 
-#     """
+from qasearch.qa_bert import *
+@app.route('/qasearch', methods=['POST'])
+def qasearch_task():
+    """
+    this search using Redis Graph to get list of articles and sentences and then calls BERT QA model to create answer
+    TODO: pre-process articles with qa tokeniser 
+    """
      
 
-#     if not request.json or not 'search' in request.json:
-#         abort(400)
-#     question=request.json['search']
-#     nodes=match_nodes(question)
-#     links,_,_=get_edges(nodes)
-#     result_table=[]
-#     for each_record in links[0:5]:  
-#         edge_query=each_record['source']+":"+each_record['target'] 
-#         print(edge_query)
-#         edge_scored=redis_client.zrangebyscore(f"edges_scored:{edge_query}",'-inf','inf',0,5)
-#         if edge_scored:
-#             for sentence_key in edge_scored:
-#                 *head,tail=sentence_key.split(':')
-#                 sentence=rediscluster_client.hget(":".join(head),tail)
-#                 article_id=head[1]
-#                 title=redis_client.hget(f"article_id:{article_id}",'title')
-#                 hash_tag=head[-1]
-#                 answer=qa(question,sentence,hash_tag)
-#             result_table.append({'title':title,'sentence':sentence,'sentencekey':sentence_key,'answer':answer})        
+    if not request.json or not 'search' in request.json:
+        abort(400)
+    question=request.json['search']
+    nodes=match_nodes(question)
+    links,_,_=get_edges(nodes)
+    result_table=[]
+    for each_record in links[0:5]:  
+        edge_query=each_record['source']+":"+each_record['target'] 
+        print(edge_query)
+        edge_scored=redis_client.zrangebyscore(f"edges_scored:{edge_query}",'-inf','inf',0,5)
+        if edge_scored:
+            for sentence_key in edge_scored:
+                *head,tail=sentence_key.split(':')
+                sentence=rediscluster_client.hget(":".join(head),tail)
+                article_id=head[1]
+                title=redis_client.hget(f"article_id:{article_id}",'title')
+                hash_tag=head[-1]
+                answer=qa(question,sentence,hash_tag)
+            result_table.append({'title':title,'sentence':sentence,'sentencekey':sentence_key,'answer':answer})        
 
-#     return jsonify({'links': links,'results':result_table}), 200
+    return jsonify({'links': links,'results':result_table}), 200
 
 
 if __name__ == "__main__":
