af9b16fa7e20ed6cbec6a69e2acae79931ba5730
Alex Mikhalev <alex@sci-blog.com>
2021-04-19 23:01:17 +0100

    Added comment
    Signed-off-by: Alex Mikhalev <alex@sci-blog.com>

--- qasearch/qa_bert.py
+++ qasearch/qa_bert.py
@@ -24,7 +24,7 @@ def loadTokeniser():
 
 
 def qa(question, sentence_key,hash_tag):
-    ### question is encoded
+    ### question 
     ### use pre-computed context/answer text tensor populated by tokeniser_gears_redisai.py
 
     global tokenizer
@@ -38,25 +38,30 @@ def qa(question, sentence_key,hash_tag):
 
     input_ids_question = tokenizer.encode(question, add_special_tokens=True, truncation=True, return_tensors="np")
 
-    num_seg_a=len(input_ids_question)
-    num_seg_b=redisai_cluster_client.tensorget(token_key,meta_only=True)['shape'][0]
-    segment_ids = np.array([0]*num_seg_a + [1]*num_seg_b)
+    input_ids_context=redisai_cluster_client.tensorget(token_key)
+    input_ids = np.append(input_ids_question,input_ids_context)
 
-    redisai_cluster_client.tensorset(f'input_ids{hash_tag}', input_ids_question)
-    # TODO: add torchscript (qa_append) to run numpy append input_ids_question and input_ids_context via torch.cat
-    redisai_cluster_client.tensorset(f'token_type_ids{hash_tag}', segment_ids)
+    attention_mask = np.array([[1]*len(input_ids)])
+    input_idss=np.array([input_ids])
+   
+    num_seg_a=input_ids_question.shape[1]
 
-    redisai_cluster_client.modelrun(f'bert-qa{hash_tag}', [f'input_ids{hash_tag}', f'token_type_ids{hash_tag}'],
+    num_seg_b=input_ids_context.shape[0]
+
+    token_type_ids = np.array([0]*num_seg_a + [1]*num_seg_b)
+        
+    redisai_cluster_client.tensorset(f'input_ids{hash_tag}', input_idss)
+    redisai_cluster_client.tensorset(f'attention_mask{hash_tag}', attention_mask)
+    redisai_cluster_client.tensorset(f'token_type_ids{hash_tag}', token_type_ids)
+
+    redisai_cluster_client.modelrun(f'bert-qa{hash_tag}', [f'input_ids{hash_tag}', f'attention_mask{hash_tag}', f'token_type_ids{hash_tag}'],
                         [f'answer_start_scores{hash_tag}', f'answer_end_scores{hash_tag}'])
-
     print(f"Model run on {hash_tag}")
     answer_start_scores = redisai_cluster_client.tensorget(f'answer_start_scores{hash_tag}')
     answer_end_scores = redisai_cluster_client.tensorget(f'answer_end_scores{hash_tag}')
 
     answer_start = np.argmax(answer_start_scores)
     answer_end = np.argmax(answer_end_scores) + 1
-
-    input_ids = inputs["input_ids"].tolist()[0]
     
     answer = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(input_ids[answer_start:answer_end]))
     return answer
@@ -64,4 +69,4 @@ def qa(question, sentence_key,hash_tag):
 if __name__ == "__main__":
     question="Effectiveness of community contact reduction"
     content_text="This would need tight coordination among pharmaceutical companies, governments, regulatory agencies, and the World Health Organization (WHO), as well as novel and out-of-the-box approaches to cGMP production, release processes, regulatory science, and clinical trial design."
-    print(qa(question,content_text,'{62n}'))
+    print(qa(question,"PMC261870.xml:{06S}:26",'{06S}'))
