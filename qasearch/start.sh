623f99df4778a78e29402801647ad7f13e78c435
Alex Mikhalev <alex@sci-blog.com>
2021-05-04 21:41:52 +0100

    Cleaned up logs and numpy conversion
    Signed-off-by: Alex Mikhalev <alex@sci-blog.com>

---
+++ qasearch/start.sh
@@ -0,0 +1,4 @@
+python export_load_bert.py
+gears-cli run --host 127.0.0.1 --port 30001 tokeniser_gears_redisai.py --requirements requirements.txt
+gears-cli run --host 127.0.0.1 --port 30001 qa_redisai_gear_map_keymiss_np.py 
+# validate by redis-cli -c -p 30001 -h 127.0.0.1 get "bertqa{5M5}_PMC140314.xml:{5M5}:44_When air samples collected?"
