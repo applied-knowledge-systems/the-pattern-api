917465b60b027eea5833f70366407b34725c27dd
Alex Mikhalev <alex@sci-blog.com>
2022-06-15 00:54:43 +0100

    Dockerized venv - no need for local venv

--- qasearch/transformers_plain_bert_qa.py
+++ qasearch/transformers_plain_bert_qa.py
@@ -46,7 +46,7 @@ t = time.process_time()
 for i in range(20):
     answer=qa(question,content_text)
     print(answer)
-elapsed_time = time.perf_counter() - t
+elapsed_time = time.process_time() - t
 print(f"Total {elapsed_time} seconds")
 print(f"{elapsed_time/20} seconds per inference")
 
